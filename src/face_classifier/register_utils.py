2f476e9127507c13db0ec6e12d55343b11d246f4
Jose B <jose.zdy@gmail.com>
2020-04-13 01:05:33 -0500

    clean code

--- src/face_classifier/register_utils.py
+++ src/face_classifier/register_utils.py
@@ -1,60 +1,63 @@
-import cv2
-import numpy as np
-import pickle
 import os
+import cv2
+import pickle
+
+import numpy as np
+
 
 def compare_embeddings(emb1t, emb2t):
     emb1 = emb1t.flatten()
     emb2 = emb2t.flatten()
     from numpy.linalg import norm
-    sim = np.dot(emb1, emb2)/(norm(emb1)*norm(emb2))
+    sim = np.dot(emb1, emb2) / (norm(emb1) * norm(emb2))
     return sim
-
-#def compare_faces(data_emb, emb):
 
 
 def scaller_conc(img):
-    oheight,owidth,_ = img.shape
+    oheight, owidth, _ = img.shape
     if(oheight >= owidth):
         height = 112
-        p = (height/oheight)
-        width = owidth*p
-        dim = (int(width), int(height) )
+        p = (height / oheight)
+        width = owidth * p
+        dim = (int(width), int(height))
         # resize image
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        nl = int((112 - resized.shape[1] )/2)
-        lines = np.zeros( (resized.shape[0],nl,3), np.uint8  )
-        resized = np.column_stack(( lines,np.column_stack(( resized, lines )) ))
-        if( ((112 - resized.shape[1])/2) != 0):
-            resized = np.column_stack(( np.zeros((resized.shape[0],1,3),np.uint8 ), resized ) )
+        resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+        nl = int((112 - resized.shape[1]) / 2)
+        lines = np.zeros((resized.shape[0], nl, 3), np.uint8)
+        resized = np.column_stack((lines, np.column_stack((resized, lines))))
+        if(((112 - resized.shape[1]) / 2) != 0):
+            resized = np.column_stack(
+                (np.zeros((resized.shape[0], 1, 3), np.uint8), resized))
         return resized
 
     elif(owidth > oheight):
         width = 112
-        p = (width/owidth)
-        height = oheight*p
-        dim = (int(width), int(height) )
+        p = (width / owidth)
+        height = oheight * p
+        dim = (int(width), int(height))
         # resize image
-        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-        nl = int((112 - resized.shape[0] )/2)
-        lines = np.zeros( (nl,resized.shape[1],3),np.uint8  )
+        resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+        nl = int((112 - resized.shape[0]) / 2)
+        lines = np.zeros((nl, resized.shape[1], 3), np.uint8)
 
-        resized = np.row_stack(( lines,np.row_stack(( resized, lines )) ))
-        if( ((112 - resized.shape[0])/2) != 0 ):
+        resized = np.row_stack((lines, np.row_stack((resized, lines))))
+        if(((112 - resized.shape[0]) / 2) != 0):
 
-            resized = np.row_stack(( np.zeros((1,resized.shape[1],3) ,np.uint8), resized ) )
+            resized = np.row_stack(
+                (np.zeros((1, resized.shape[1], 3), np.uint8), resized))
         return resized
 
     else:
         return img
 
-def update_model_face(recognizer,WIDTHDIVIDER = 4):
 
-
-   
+def update_model_face(recognizer):
     path = "dataset/"
 
-    embeddings = np.zeros( (1,512) )
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    embeddings = np.zeros((1, 512))
     names = []
     unames = []
 
@@ -62,29 +65,20 @@ def update_model_face(recognizer,WIDTHDIVIDER = 4):
         unames.append(folder)
         faces = []
         for img_name in os.listdir(path + folder + '/'):
-
-            face = cv2.imread(path + folder + '/' + img_name)           
-            
+            face = cv2.imread(path + folder + '/' + img_name)
             face = scaller_conc(face)
             faces.append(face)
 
-        if(faces):
+        if faces:
             for face in faces:
-                if(face is not None):
-                    embeddings = np.row_stack(( embeddings,recognizer.get_embedding(face)  ))
+                if face is not None:
+                    embeddings = np.row_stack(
+                        (embeddings, recognizer.get_embedding(face)))
                     names.append(folder)
-                    print('File Coded: ', folder)
 
-            
-
-
-    #embeddings = np.delete(embeddings , 0, 0)
-
-    print(embeddings.shape)
-
-    with open('database/'+ 'embeddings.pickle', 'wb') as f:
+    with open('database/' + 'embeddings.pickle', 'wb') as f:
         pickle.dump(embeddings, f)
-    with open('database/'+ 'names.pickle', 'wb') as f:
+    with open('database/' + 'names.pickle', 'wb') as f:
         pickle.dump(names, f)
-    with open('database/'+ 'unames.pickle', 'wb') as f:
+    with open('database/' + 'unames.pickle', 'wb') as f:
         pickle.dump(unames, f)
--- src/flask_api.py
+++ src/flask_api.py
@@ -110,52 +110,33 @@ def update_model():
     not_busy()
     return resp
 
-###############################################
-
 
 @app.route('/register_face/', methods=['POST'])
 def register_face():
-    # check if the post request has the file part
-
-    name = json.loads(request.form.get('json'))["face_name"]
-
-    print(name)
-    if is_busy():
-        resp = jsonify({'message': 'Service is being used'})
-        resp.status_code = 503
-        return resp
-
     if 'file' not in request.files:
         resp = jsonify({'message': 'No file part in the request'})
         resp.status_code = 400
-        not_busy()
         return resp
 
     file_1 = request.files['file']
-
     if file_1.filename == '':
         resp = jsonify({'message': 'No file selected for uploading'})
         resp.status_code = 400
-        not_busy()
         return resp
 
-    if file_1 and allowed_file(file_1.filename):
-        filename = secure_filename(file_1.filename)
-
-        file_1.save(os.path.join("./", filename))
-
-        resp = jsonify({'message': "Success upload"})
-        resp.status_code = 200
-        not_busy()
-        return resp
-
-    else:
+    if not file_1 or not allowed_file(file_1.filename):
         resp = jsonify({
             'message': f'Allowed file types are {ALLOWED_EXTENSIONS}'
         })
         resp.status_code = 400
-        not_busy()
         return resp
+
+    filename = secure_filename(file_1.filename)
+    file_1.save(os.path.join("./", filename))
+
+    resp = jsonify({'message': "Success upload"})
+    resp.status_code = 200
+    return resp
 
 
 if __name__ == "__main__":
